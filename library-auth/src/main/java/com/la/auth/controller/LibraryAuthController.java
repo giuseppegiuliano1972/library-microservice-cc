bfbf1927c4b4a8c04559afd0833ba2fe192638f1
Giuseppe Giuliano <ggiuliano1972@hotmail.it>
2023-07-03 18:51:09 +0200

    update

--- library-auth/src/main/java/com/la/auth/controller/LibraryAuthController.java
+++ library-auth/src/main/java/com/la/auth/controller/LibraryAuthController.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -13,6 +14,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -103,5 +105,15 @@ public class LibraryAuthController {
 
 		return ResponseEntity.ok(new ErrorResponse("User registered successfully!", null));
 	}
+	
+	@GetMapping("/health")
+    public ResponseEntity<Long> getHealth() {
+
+        
+        return new ResponseEntity<>(
+        		1L,
+                HttpStatus.OK
+        );
+    }
 
 }
